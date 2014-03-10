a02b731c356ff15efa88eade0881aedf34af53c7
Sam Blackman <sblackman@gmail.com>
2014-03-10 12:58:36 -0500

    added devise user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortner2::Application.config.secret_key_base = '20dc4f7b95a194a7aa13d89a1818ef50508df290cceecef7732f59dbe7fdd696217c6221a905d3e5428c89888a5dec0675202b644817da201d5a9724b9b15474'
